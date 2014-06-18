90ebe45e97fa915a0c6f0c52fbb7975e00fc75d6
Hackbright Student <info@hackbrightacademy.com>
2014-06-17 17:37:38 -0700

    Initial commit for SQL lesson, used to work, then we added get_grades, now it does nothing!

--- hackbright_app.py
+++ hackbright_app.py
@@ -16,19 +16,59 @@ def connect_to_db():
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s" %(first_name, last_name)
+
+def get_project_description(project_title):
+    query = """SELECT title, description FROM Projects WHERE title=(?) """
+    DB.execute(query, (project_title,))
+    row = DB.fetchone()
+    print """\
+Project: %s 
+Project Description: %s""" % (row[0], row[1])
+
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT INTO Projects (title, description, max_grade) VALUES (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added Project: %s" %title
+
 def main():
     connect_to_db()
     command = None
+    argument_list = []
+    
     while command != "quit":
         input_string = raw_input("HBA Database> ")
-        tokens = input_string.split()
-        command = tokens[0]
-        args = tokens[1:]
+        temp_arg_list = input_string.split()
+        if temp_arg_list[0]=="new_project":
+            new_input_string = input_string.split("\"")
+            first_two_args = new_input_string[0].split()
+
+            first_two_args.append(new_input_string[1])
+            first_two_args.append(new_input_string[2])
+    
+            argument_list = first_two_args
+            command = argument_list[0]
+            args = argument_list[1:]
+
+        else:
+            tokens = input_string.split()
+            command = tokens[0]
+            args = tokens[1:]
 
         if command == "student":
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "Projects":
+            get_project_description(*args) 
+        elif command == "new_project":
+            make_new_project(*args)   
 
     CONN.close()
 
